7634b29a85bf833d6f3e52a46d35c46b8675f0e8
JohannesIBK <gg.johannes.j@gmail.com>
2022-01-27 14:21:01 +0100

    fix imports

--- dashboard/api/src/api/client/client.module.ts
+++ dashboard/api/src/api/client/client.module.ts
@@ -1,17 +1,29 @@
+import { HttpModule } from "@nestjs/axios";
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { EntryEntity } from "../../database/entry.entity";
+import { PhaseEntity } from "../../database/phase.entity";
 import { PlayerEntity } from "../../database/player.entity";
 import { StatsEntity } from "../../database/stats.entity";
+import { TeamEntity } from "../../database/team.entity";
+import { TournamentEntity } from "../../database/tournament.entity";
 import { UserEntity } from "../../database/user.entity";
+import { MojangService } from "../../services/mojang.service";
+import { PhaseService } from "../../services/phase.service";
 import { PlayerService } from "../../services/player.service";
 import { SocketService } from "../../services/socket.service";
 import { StatsService } from "../../services/stats.service";
+import { TeamService } from "../../services/team.service";
+import { TournamentService } from "../../services/tournament.service";
 import { UserService } from "../../services/user.service";
 import { ClientController } from "./client.controller";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity, StatsEntity, PlayerEntity])],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity, StatsEntity, PlayerEntity, TeamEntity, PhaseEntity, EntryEntity, TournamentEntity]),
+    HttpModule,
+  ],
   controllers: [ClientController],
-  providers: [UserService, StatsService, PlayerService, SocketService],
+  providers: [UserService, StatsService, PlayerService, SocketService, TeamService, MojangService, PhaseService, TournamentService],
 })
 export class ClientModule {}
